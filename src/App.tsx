3972759a31dfb659dccd2c221f518f0f81a56988
Ancentian <ancentmbithi8@gmail.com>
2023-12-04 16:05:35 +0300

    Alerts, State, Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
+//import ListGroup from "./components/ListGroup";
+
+// function App() {
+
+  // let items = ["Nairobi", "Mombasa", "Kisumu", "Nakuru", "Eldoret", "Machakos"];
+
+  // const handleSelectedItem = (item: string) => {
+  //   console.log(item);
+  // };
+
+  // return (
+  //   <div>
+  //     <ListGroup items={items} heading="Cities" onSelectItem={ handleSelectedItem}/>
+  //   </div>
+  // );
+
+
+//   function App() {
+//     return (
+//     <div>
+//       <Alert>
+//         Hello <span>Ancent</span>
+//       </Alert>  
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Alert>
+//         Hello <span>Ancent</span>
+//       </Alert>
+//     </div>
+//   );
+// }
 
 function App() {
-  const items = [
-    'Nairobi', 
-    'Mombasa',
-    'Kisumu',
-    'Nakuru',
-    'Eldoret',
-    'Machakos'
-  ]
+  const [alertVisible, setAlertVisiblility] = useState(false);
+
   return (
-    <>
-      <h1>List</h1>
-      <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </>
+    <div>
+      {alertVisible && <Alert onClose={() => setAlertVisiblility(false)}>My Alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisiblility(true)}>
+        My Button
+      </Button>
+    </div>
   );
 }
 
